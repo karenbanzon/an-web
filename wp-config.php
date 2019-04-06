5787567f0259e48db1c1054be6ba236252ce07f3
Karen Banzon <kabanzon@gmail.com>
2019-04-06 15:46:47 +0200

    Updated wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -79,6 +79,9 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+// Define uploads folder
+define( 'UPLOADS', 'wp-content/uploads' );
+
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
