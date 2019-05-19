61d463e88fc1f8dc86b76e2a4007c13c1114a9b6
Karen Banzon <kabanzon@gmail.com>
2019-05-19 18:21:32 +0200

    Added metaboxes for events and page display for event pages

--- wp-content/themes/an-theme/functions.php
+++ wp-content/themes/an-theme/functions.php
@@ -543,3 +543,164 @@ class Documents_Meta_Box {
 	}
 }
 new Documents_Meta_Box;
+
+
+/* Add Events metadata */
+
+function events_metaboxes() {
+	add_meta_box( 'an_event_date_start', 'Start Date and Time', 'an_event_date', 'events', 'normal', 'default', array( 'id' => '_start') );
+	add_meta_box( 'an_event_date_end', 'End Date and Time', 'an_event_date', 'events', 'normal', 'default', array('id'=>'_end') );
+	add_meta_box( 'an_event_location', 'Event Location', 'an_event_location', 'events', 'normal', 'default', array('id'=>'_end') );
+}
+add_action( 'admin_init', 'events_metaboxes' );
+
+// Metabox HTML
+
+function an_event_date($post, $args) {
+	$metabox_id = $args['args']['id'];
+	global $post, $wp_locale;
+
+	// Use nonce for verification
+	wp_nonce_field( plugin_basename( __FILE__ ), 'ep_eventposts_nonce' );
+
+	$time_adj = current_time( 'timestamp' );
+	$month = get_post_meta( $post->ID, $metabox_id . '_month', true );
+
+	if ( empty( $month ) ) {
+			$month = gmdate( 'm', $time_adj );
+	}
+
+	$day = get_post_meta( $post->ID, $metabox_id . '_day', true );
+
+	if ( empty( $day ) ) {
+			$day = gmdate( 'd', $time_adj );
+	}
+
+	$year = get_post_meta( $post->ID, $metabox_id . '_year', true );
+
+	if ( empty( $year ) ) {
+			$year = gmdate( 'Y', $time_adj );
+	}
+
+	$hour = get_post_meta($post->ID, $metabox_id . '_hour', true);
+
+	if ( empty($hour) ) {
+			$hour = gmdate( 'H', $time_adj );
+	}
+
+	$min = get_post_meta($post->ID, $metabox_id . '_minute', true);
+
+	if ( empty($min) ) {
+			$min = '00';
+	}
+
+	$month_s = '<select name="' . $metabox_id . '_month">';
+	for ( $i = 1; $i < 13; $i = $i +1 ) {
+			$month_s .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
+			if ( $i == $month )
+					$month_s .= ' selected="selected"';
+			$month_s .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
+	}
+	$month_s .= '</select>';
+
+	echo $month_s;
+	echo '<input type="text" name="' . $metabox_id . '_day" value="' . $day  . '" size="2" maxlength="2" />';
+	echo '<input type="text" name="' . $metabox_id . '_year" value="' . $year . '" size="4" maxlength="4" /> @ ';
+	echo '<input type="text" name="' . $metabox_id . '_hour" value="' . $hour . '" size="2" maxlength="2"/>:';
+	echo '<input type="text" name="' . $metabox_id . '_minute" value="' . $min . '" size="2" maxlength="2" />';
+
+}
+
+function an_event_location() {
+	global $post;
+	// Use nonce for verification
+	wp_nonce_field( plugin_basename( __FILE__ ), 'ep_eventposts_nonce' );
+	// The metabox HTML
+	$event_location = get_post_meta( $post->ID, '_event_location', true );
+	echo '<label for="_event_location">Location:</label>';
+	echo '<input type="text" name="_event_location" value="' . $event_location  . '" />';
+}
+
+// Save the Metabox Data
+
+function ep_eventposts_save_meta( $post_id, $post ) {
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return;
+
+	if ( !isset( $_POST['ep_eventposts_nonce'] ) )
+			return;
+
+	if ( !wp_verify_nonce( $_POST['ep_eventposts_nonce'], plugin_basename( __FILE__ ) ) )
+			return;
+
+	// Is the user allowed to edit the post or page?
+	if ( !current_user_can( 'edit_post', $post->ID ) )
+			return;
+
+	// OK, we're authenticated: we need to find and save the data
+	// We'll put it into an array to make it easier to loop though
+
+	$metabox_ids = array( '_start', '_end' );
+
+	foreach ($metabox_ids as $key ) {
+			$events_meta[$key . '_month'] = $_POST[$key . '_month'];
+			$events_meta[$key . '_day'] = $_POST[$key . '_day'];
+					if($_POST[$key . '_hour']<10){
+							 $events_meta[$key . '_hour'] = '0'.$_POST[$key . '_hour'];
+					 } else {
+								 $events_meta[$key . '_hour'] = $_POST[$key . '_hour'];
+					 }
+			$events_meta[$key . '_year'] = $_POST[$key . '_year'];
+			$events_meta[$key . '_hour'] = $_POST[$key . '_hour'];
+			$events_meta[$key . '_minute'] = $_POST[$key . '_minute'];
+			$events_meta[$key . '_eventtimestamp'] = $events_meta[$key . '_year'] . $events_meta[$key . '_month'] . $events_meta[$key . '_day'] . $events_meta[$key . '_hour'] . $events_meta[$key . '_minute'];
+	}
+
+	$events_meta['_event_location'] = $_POST['_event_location'];
+
+	// Add values of $events_meta as custom fields
+
+	foreach ( $events_meta as $key => $value ) { // Cycle through the $events_meta array!
+			if ( $post->post_type == 'revision' ) return; // Don't store custom data twice
+			$value = implode( ',', (array)$value ); // If $value is an array, make it a CSV (unlikely)
+			if ( get_post_meta( $post->ID, $key, FALSE ) ) { // If the custom field already has a value
+					update_post_meta( $post->ID, $key, $value );
+			} else { // If the custom field doesn't have a value
+					add_post_meta( $post->ID, $key, $value );
+			}
+			if ( !$value ) delete_post_meta( $post->ID, $key ); // Delete if blank
+	}
+
+}
+
+add_action( 'save_post', 'ep_eventposts_save_meta', 1, 2 );
+
+/**
+* Helpers to display the date on the front end
+*/
+
+// Get the Month Abbreviation
+
+function eventposttype_get_the_month_abbr($month) {
+	global $wp_locale;
+	for ( $i = 1; $i < 13; $i = $i +1 ) {
+							if ( $i == $month )
+									$monthabbr = $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) );
+							}
+	return $monthabbr;
+}
+
+// Display the date
+
+function eventposttype_get_the_event_date() {
+	global $post;
+	$eventdate = '';
+	$month = get_post_meta($post->ID, '_month', true);
+	$eventdate = eventposttype_get_the_month_abbr($month);
+	$eventdate .= ' ' . get_post_meta($post->ID, '_day', true) . ',';
+	$eventdate .= ' ' . get_post_meta($post->ID, '_year', true);
+	$eventdate .= ' at ' . get_post_meta($post->ID, '_hour', true);
+	$eventdate .= ':' . get_post_meta($post->ID, '_minute', true);
+	echo $eventdate;
+}
---
+++ wp-content/themes/an-theme/page-templates/workshops.php
@@ -0,0 +1,59 @@
+<?php /* Template Name: Webinars */ ?>
+
+<?php get_header(); ?>
+<main id="content" class="page-content flex flex-wrap justify-center w-full">
+  <section class="flex w-full">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('w-full'); ?>>
+      <?php if ( has_post_thumbnail() ) { ?>
+      <header class="header page-hero p-6" style="background-image: url(<?php echo the_post_thumbnail_url(); ?>)">
+        <div class="overlay bg-anblue"></div>
+        <div class="page-title"><h1 class="entry-title w-full ml-auto mr-auto mt-4 mb-4 pt-6 pb-6 text-white"><?php the_title(); ?></h1></div>
+      </header>
+      <?php } else { ?>
+      <header class="header w-full m-auto p-6">
+        <h1 class="entry-title pt-4 pb-4 text-grey-darkest"><?php the_title(); ?></h1>
+      </header>
+      <?php } ?>
+      <div class="w-full m-auto p-6">
+        <?php the_content(); ?>
+      </div>
+      <?php endwhile; endif; ?>
+
+      <hr class="w-full border-b border-grey">
+        
+      <!-- Webinars -->
+      <div class="flex flex-wrap w-full m-auto p-6">
+      <?php
+        $regular = new WP_Query( array(
+          'post_type' => 'events',
+          'category_name' => 'workshop'
+        ) );
+
+        if ( $regular->have_posts() ) : ?>
+        
+        <?php while ( $regular->have_posts() ) : $regular->the_post(); ?>
+
+        <div class="w-full lg:w-1/3 p-6 hover:shadow">
+          <a href="<?php echo the_permalink(); ?>">
+            <?php if ( has_post_thumbnail() ) { ?>
+              <div class="bg-cover bg-center mb-2" style="background-image:url(<?php echo the_post_thumbnail_url(); ?>); height: 300px;">
+              </div>
+            <?php } ?>
+            <h4 class="text-black"><?php the_title(); ?></h4>
+            <?php $post_meta = get_post_meta( get_the_ID() ); ?>
+            <?php echo '<script>console.log(' . json_encode($post_meta) . ')</script>' ?>
+            <small class="text-grey"><?php echo $post_meta['_start_day'][0] . '.' . $post_meta['_start_month'][0] . '.' . $post_meta['_start_year'][0] ?></small>
+            <br>
+            <small class="text-grey"><?php echo $post_meta['_event_location'][0] ?></small>
+          </a>
+        </div>
+
+        <?php endwhile; ?>
+        
+        <?php endif; wp_reset_postdata();?>
+      </div>
+    </article>
+  </section>
+</main>
+<?php get_footer(); ?>
