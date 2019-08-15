d00c2dc15d1504d66a6252c8a6ca9baecfa1bc31
Karen Banzon <kabanzon@gmail.com>
2019-08-15 11:15:37 +0200

    Show debug for resources

--- wp-content/themes/an-theme/page-templates/resources.php
+++ wp-content/themes/an-theme/page-templates/resources.php
@@ -127,7 +127,7 @@
               <?php
                 $args = array(
                   'numberposts' => 1,
-                  'category' => $mems[0]['term_id'],
+                  'category' => $mems[0]['id'],
                   'post_type' => 'members'
                 );
 
@@ -136,7 +136,8 @@
 
               ?>
               <?php if ( get_the_post_thumbnail_url($currentOrg[0]->ID) ) { ?>
-                <span><?php echo $currentOrg[0]->ID; ?></span>
+                <span><?php echo json_encode($mems) ?></span>
+                <span><?php echo json_encode($currentOrg) ?></span>
                 <img class="w-16" src="<?php echo get_the_post_thumbnail_url($currentOrg[0]->ID); ?>" alt="<?php echo $currentOrg[0]->post_title; ?>">
               <?php } else { ?>
                 <span class="inline-block align-middle text-grey text-sm py-2"><?php echo $currentOrg[0]->post_title; ?></span>
