534e15d88017735a3b068b50b2628ff07f2597f1
Karen Banzon <kabanzon@gmail.com>
2019-08-15 10:08:59 +0200

    Added page template with no header

---
+++ wp-content/themes/an-theme/page-templates/page.php
@@ -0,0 +1,26 @@
+<?php /* Template Name: Default - No title header */ ?>
+
+<?php get_header(); ?>
+<main id="content" class="page-content flex flex-wrap justify-center w-full">
+  <section class="flex w-full">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('w-full'); ?>>
+      <?php if ( has_post_thumbnail() ) { ?>
+      <header class="hidden header page-hero p-6" style="background-image: url(<?php echo the_post_thumbnail_url(); ?>)">
+        <div class="overlay bg-anblue"></div>
+        <div class="page-title"><h1 class="entry-title w-full ml-auto mr-auto mt-4 mb-4 pt-6 pb-6 text-white"><?php the_title(); ?></h1></div>
+      </header>
+      <?php } else { ?>
+      <header class="hidden header w-full m-auto p-6">
+        <h1 class="entry-title pt-4 pb-4 text-grey-darkest"><?php the_title(); ?></h1>
+      </header>
+      <?php } ?>
+      <div class="w-full m-auto p-6">
+        <?php the_content(); ?>
+      </div>
+      <?php endwhile; endif; ?>
+    </article>
+      
+  </section>
+</main>
+<?php get_footer(); ?>
