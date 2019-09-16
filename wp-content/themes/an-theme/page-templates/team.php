6bd0830ecf06963530e72bdfa7278002e0683d2a
Karen Banzon <kabanzon@gmail.com>
2019-09-16 12:12:59 +0200

    Update modal to support all team caegories

--- wp-content/themes/an-theme/page-templates/team.php
+++ wp-content/themes/an-theme/page-templates/team.php
@@ -150,7 +150,7 @@
   </section>
 </main>
 <div id="member-modal-container" class="hidden fixed flex-col w-full h-full items-center p-4 md:mt-4" style="background-color: rgba(0,0,0,.75); top: 0">
-  <div id="member-modal" class="hidden modal w-full md:w-1/3 h-full md:h-auto z-50 absolute inset-auto bg-white shadow mt-8 p-8">
+  <div id="member-modal" class="hidden modal w-full lg:w-1/3 md:w-3/4 h-full md:h-auto z-50 absolute inset-auto bg-white shadow mt-8 p-8">
     <span id="member-modal-close" class="absolute cursor-pointer" style="top: 16px; right: 16px">
       <svg class="svg-icon" viewBox="0 0 20 20" width=28>
         <path fill="auto" d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
