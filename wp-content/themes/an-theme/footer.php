698915769e2bdb5851de97e60ea94fd1265ad3c0
Karen Banzon <kabanzon@gmail.com>
2019-10-23 09:58:53 +0200

    Add footer

--- wp-content/themes/an-theme/footer.php
+++ wp-content/themes/an-theme/footer.php
@@ -1,5 +1,117 @@
     </section>
 
+    <section class="flex flex-wrap w-full bg-grey-lighter mt-4">
+      <div class="flex flex-wrap content-start w-full lg:w-1/3 p-4">
+        <h3 class="w-full my-2">Sign up for our newsletter!</h3>
+        <input
+          id="newslettter_signup"
+          name="newsletter_signup"
+          type="email"
+          class="rounded border border-solid border-grey my-2 w-full h-8 p-2"
+          placeholder="e.g. mail@server.com"
+        >
+        <button class="bg-anblue border border-anblue hover:border-anblue-dark hover:bg-anblue-dark text-white font-semibold block text-center mt-2 py-2 px-2 rounded">
+          Sign up
+        </button>
+      </div>
+      <div class="flex flex-wrap content-start w-full lg:w-1/3 p-4">
+        <div class="flex w-full text-center m-4">
+          <a href="" class="w-full">
+            <svg width="32" height="32" data-name="facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.24 64">
+              <title>Facebook</title>
+              <path data-name="icon" d="M21.57,64V34.81h9.8l1.47-11.38H21.57V16.17c0-3.3.92-5.54,5.64-5.54h6V.45A80.21,80.21,0,0,0,24.46,0C15.77,0,9.82,5.3,9.82,15v8.39H0V34.81H9.82V64Z" style="fill:#000000"></path>
+            </svg>
+          </a>
+          <a href="" class="w-full">
+            <svg width="32" height="32" data-name="twitter" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77 64">
+              <title>Twitter</title>
+              <path data-name="icon" d="M75.79,7.49a31.22,31.22,0,0,1-5.31,1.77,16.15,16.15,0,0,0,4.37-7h0c.21-.73-.63-1.46-1.25-1h0a32.52,32.52,0,0,1-8.12,3.33c-.21,0-.31.1-.52.1a2.47,2.47,0,0,1-1.45-.52A16.58,16.58,0,0,0,52.47,0a18.82,18.82,0,0,0-5.3.83A16,16,0,0,0,36.34,12.38a17.65,17.65,0,0,0-.41,6.14,1,1,0,0,1-.11.42.81.81,0,0,1-.41.21H35.3a44,44,0,0,1-30-16h0a.87.87,0,0,0-1.45.11h0a16.62,16.62,0,0,0-2.29,8.32,16.44,16.44,0,0,0,5,11.86,13.8,13.8,0,0,1-3.64-1.35h0a.83.83,0,0,0-1.25.73h0a16.67,16.67,0,0,0,9.68,15.3h-.31a13.36,13.36,0,0,1-2.81-.31h0a1,1,0,0,0-1,1.14h0A16.65,16.65,0,0,0,20.32,50.26,30.54,30.54,0,0,1,3.15,55.47H1.27A1.32,1.32,0,0,0,0,56.4a1.39,1.39,0,0,0,.62,1.46A45.46,45.46,0,0,0,23.54,64a45.2,45.2,0,0,0,19.57-4.16A43.72,43.72,0,0,0,57.47,49.12a46.59,46.59,0,0,0,8.74-14.67,45.51,45.51,0,0,0,3-16v-.21a2.94,2.94,0,0,1,1-2.18,29.39,29.39,0,0,0,6.56-7.29h0a.83.83,0,0,0-1-1.25Z" transform="translate(0)" style="fill:#000000"></path>
+            </svg>
+          </a>
+          <a href="" class="w-full">
+            <svg width="32" height="32" data-name="linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66.97 64">
+              <title>LinkedIn</title>
+              <path data-name="icon" d="M0,7.43A7,7,0,0,1,2.26,2.1,8.31,8.31,0,0,1,8.15,0,8,8,0,0,1,13.9,2.07a7.27,7.27,0,0,1,2.26,5.56A6.79,6.79,0,0,1,14,12.8,8.3,8.3,0,0,1,8,14.93H8A7.9,7.9,0,0,1,2.2,12.8,7.15,7.15,0,0,1,0,7.43ZM.84,64V20.82H15.19V64Zm22.3,0H37.49V39.89A9.35,9.35,0,0,1,38,36.4a9.39,9.39,0,0,1,2.75-3.72,7,7,0,0,1,4.62-1.52q7.24,0,7.24,9.76V64H67V39.24q0-9.57-4.52-14.51a15.43,15.43,0,0,0-12-4.95,14.68,14.68,0,0,0-13,7.18v.13h-.06l.06-.13V20.82H23.14q.13,2.07.13,12.86T23.14,64Z" style="fill:#000000"></path>
+            </svg>
+          </a>
+        </div>
+        <small class="flex text-grey w-full m-4">
+          <em>Accountable Now is legally registered in United Kingdom at Amnesty International, 1 Easton Street, London WC1X 0DW, UK under the name International NGO Charter of Accountability Ltd.</em>
+        </small>
+      </div>
+      <div class="flex w-full lg:w-1/3 p-4">
+        <nav id="responsive-nav" class="w-full">
+          <ul class="list-reset">
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/about-us/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue"><strong>About us</strong><span class="nav-dropdown-toggle text-anblue px-2">&darr;</span></a>
+              <ul class="nav-dropdown list-reset ml-2 bg-white text-sm hidden">
+                <!-- <a href="<?php echo get_home_url() ?>/about-us/dynamic-accountability/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Dynamic Accountability</a> -->
+                <a href="<?php echo get_home_url() ?>/about-us/our-strategy/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Our strategy</a>
+                <a href="<?php echo get_home_url() ?>/about-us/our-members/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Our members</a>
+                <!-- <a href="<?php echo get_home_url() ?>/about-us/governance/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Governance</a> -->
+                <a href="<?php echo get_home_url() ?>/about-us/team-and-governance/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Team</a>
+                <!-- <a href="<?php echo get_home_url() ?>/about-us/annual-general-meeting/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Annual general meeting</a> -->
+              </ul>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/our-work/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue"><strong>Our work</strong><span class="nav-dropdown-toggle text-anblue px-2">&darr;</span></a>
+              <ul class="nav-dropdown list-reset ml-2 bg-white text-sm hidden">
+                <a href="<?php echo get_home_url() ?>/our-work/our-approach-to-accountability/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Our approach to accountability</a>
+                <!-- <a href="<?php echo get_home_url() ?>/our-work/12-accountability-commitments/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">12 accountability commitments</a> -->
+                <a href="<?php echo get_home_url() ?>/our-work/reporting/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Reporting</a>
+                <!-- <a href="<?php echo get_home_url() ?>/our-work/feedback-and-complaints/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Feedback & complaints</a> -->
+                <a href="<?php echo get_home_url() ?>/our-work/projects-and-partnerships/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Projects & partnerships</a>
+              </ul>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/our-accountability/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue" onclick="return false"><strong>Our accountability</strong><span class="nav-dropdown-toggle text-anblue px-2">&darr;</span></a>
+              <ul class="nav-dropdown list-reset ml-2 bg-white text-sm hidden">
+                <a href="<?php echo get_home_url() ?>/our-accountability/our-policies-and-finance/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Values &amp; policies</a>
+                <a href="<?php echo get_home_url() ?>/our-accountability/audits-finances-reports/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Reports, finances &amp; audits</a>
+                <a href="<?php echo get_home_url() ?>/our-accountability/feedback-and-complaints/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Feedback & complaints</a>
+              </ul>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/members-corner/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue"><strong>Member's corner</strong><span class="nav-dropdown-toggle text-anblue px-2">&darr;</span></a>
+              <ul class="nav-dropdown list-reset ml-2 bg-white text-sm hidden">
+                <a href="<?php echo get_home_url() ?>/members-corner/webinars/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Webinars</a>
+                <a href="<?php echo get_home_url() ?>/members-corner/workshops/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Workshops</a>
+                <a href="<?php echo get_home_url() ?>/members-corner/peer-advice-groups/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Peer advice groups</a>
+                <a href="<?php echo get_home_url() ?>/members-corner/good-practice-library/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Good practice library</a>
+                <a href="<?php echo get_home_url() ?>/members-corner/annual-general-meeting/" class="block p-2 text-grey hover:text-anblue hover:bg-grey-lighter">Annual General Meeting</a>
+              </ul>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/blog/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue">
+                <strong>Blog</strong>
+              </a>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/careers/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue">
+                <strong>Careers</strong>
+              </a>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/contact-us/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue">
+                <strong>Contact us</strong>
+              </a>
+            </li>
+            <li class="nav-item group block px-2">
+              <a href="<?php echo get_home_url() ?>/privacy-policy/" class="flex justify-between block w-full lg:w-auto lg:flex px-2 py-2 text-grey-darker hover:text-anblue group-hover:text-anblue">
+                <strong>Privacy Policy</strong>
+              </a>
+            </li>
+            <a href="<?php echo get_home_url() ?>/join-us/" class="bg-anblue border border-anblue hover:border-anblue-dark hover:bg-anblue-dark text-white font-semibold block text-center mt-2 py-2 px-2 rounded">
+              Join us
+            </a>
+            <!-- <li class="nav-item group block px-2 py-2">
+              <a href="<?php echo get_home_url() ?>/latest/" class="block w-full lg:w-auto lg:flex pt-6 lg:pt-1 lg:pl-2 text-grey hover:text-anblue group-hover:text-anblue">The latest</a>
+            </li> -->
+          </ul>
+        </nav>
+      </div>
+    </section>
+
     <footer id="footer" class="w-full p-6 border-t-2 border-grey-light">
       <div id="copyright" class="text-center text-grey">
         &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
