149df57fe72179f79ec2a1ab2d2e3f788c1e1920
Karen Banzon <kabanzon@gmail.com>
2020-03-04 15:46:16 +0100

    Testing nginx conf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(accountablenow.org www.accountablenow.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="info@accountablenow.org" # Adding a valid address is strongly recommended
+email="comms@accountablenow.org" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
